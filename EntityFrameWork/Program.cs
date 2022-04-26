ac99ee8992951fed87040216777e248e535b9872
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-04-26 18:40:50 +0400

    Add files via upload

---
+++ EntityFrameWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EntityFrameWork.EmployeeController;
+namespace EntityFrameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpController employee = new EmpController();
+            // employee.AddEmployee("Mark");
+            // employee.AddEmployee("Anna");
+            //employee.AddEmployee("Tom");
+            // employee.DeleteEmployee(2);
+            //employee.GetAll();
+            // employee.FilterByName("a");
+            //employee.GetEmployeeById(8);
+           
+        }
+    }
+}
